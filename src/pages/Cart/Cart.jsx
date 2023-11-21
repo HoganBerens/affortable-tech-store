395083d375ebd620f0ee80c0b9faa8ee4b231875
HoganBerens <berenshogan@gmail.com>
2023-11-21 08:15:53 -0600

    get categories and items

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -2,6 +2,10 @@ import "./Cart.css";
 import { React } from "react";
 
 const Cart = () => {
-  return <div className="cart-container"></div>;
+  return (
+    <div className="cart-container">
+      <div></div>
+    </div>
+  );
 };
 export default Cart;
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
 import "./Dashboard.css";
 import { React } from "react";
 
-const Dashboard = () => {
-  return <div className="dashboard-container"></div>;
+const Dashboard = (props) => {
+  let items = props.items;
+  let categories = props.categories;
+
+  return (
+    <div className="dashboard-container">
+      <div>
+        {items.map((item, itemIndex) => (
+          <div key={itemIndex}>{item.name}</div>
+        ))}
+
+        {categories.map((cat, catIndex) => (
+          <div key={catIndex}>{cat.name}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default Dashboard;
